aab0667d0f40c0ad6fcbf9d812eab97f96f50444
psbharathy <subramania.bharathy@compassitesinc.com>
2018-08-14 14:42:13 +0530

    proof single source of truth with Reset

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,6 +10,14 @@ class Counters extends Component {
     ]
   };
 
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
   handleDelete = counterId => {
     // console.log("Event Handled", counterId);
     const counters = this.state.counters.filter(c => c.id !== counterId);
@@ -20,6 +28,9 @@ class Counters extends Component {
     // Key is special word but value is arg
     return (
       <div>
+        <button onClick={this.handleReset} className="btn-primary btn-sm m-2">
+          Reset
+        </button>
         {this.state.counters.map(counter => (
           <Counter
             key={counter.id}
