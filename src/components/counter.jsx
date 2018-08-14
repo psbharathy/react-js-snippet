15a26d2da2faac620a1fd57246e3abd3f60662bf
psbharathy <subramania.bharathy@compassitesinc.com>
2018-08-14 14:31:29 +0530

    update the State

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,7 +4,7 @@ class Counter extends Component {
   // State is Local to class
 
   state = {
-    value: this.props.value
+    value: this.props.counter.value
   };
 
   // constructor() {
@@ -35,7 +35,7 @@ class Counter extends Component {
           Increment
         </button>
         <button
-          onClick={this.props.onDelete}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn btn-danger btn-sm m-2"
         >
           Delete
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,8 +10,10 @@ class Counters extends Component {
     ]
   };
 
-  handleDelete = () => {
-    console.log("Event Handled");
+  handleDelete = counterId => {
+    // console.log("Event Handled", counterId);
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters: counters });
   };
 
   render() {
@@ -22,7 +24,7 @@ class Counters extends Component {
           <Counter
             key={counter.id}
             onDelete={this.handleDelete}
-            value={counter.value}
+            counter={counter}
           />
         ))}
       </div>
